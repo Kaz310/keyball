a39d82621ba1298f8c2bd434479a9182d6cfd72b
Kaz310 <ptarmigan.09.02@gmail.com>
2025-07-24 22:42:43 +0900

    fix error mine

--- qmk_firmware/keyboards/keyball/keyball44/keymaps/mine/keymap.c
+++ qmk_firmware/keyboards/keyball/keyball44/keymaps/mine/keymap.c
@@ -69,7 +69,7 @@ bool process_record_user(uint16_t keycode, keyrecord_t *record)
       case HOLDEN:
         unregister_ctrl();
         break;
-      case default:
+      case RELEASED:
         break;
       }
       ctrl_alt_state = RELEASED;
