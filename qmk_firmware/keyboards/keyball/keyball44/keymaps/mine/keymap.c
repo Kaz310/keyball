dc68df708e3494e768867ba84eac0e6c4191ee12
Kaz310 <ptarmigan.09.02@gmail.com>
2025-07-27 01:26:01 +0900

    fix key map of mine

--- qmk_firmware/keyboards/keyball/keyball44/keymaps/mine/keymap.c
+++ qmk_firmware/keyboards/keyball/keyball44/keymaps/mine/keymap.c
@@ -21,6 +21,9 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 #include "quantum.h"
 
 // custom keys
+#define LYR_SPC LT(3, KC_SPC)
+#define LYR_MINS LT(2, KC_MINS)
+#define LYR_ENT LT(1, KC_ENT)
 #define SFT_Z SFT_T(KC_Z)
 #define SFT_GRV SFT_T(KC_GRV)
 #define SFT_1 SFT_T(KC_1)
@@ -49,6 +52,7 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 #define COPY CTL_T(KC_C)
 #define PASTE CTL_T(KC_V)
 #define CNG_WIN GUI_T(KC_TAB)
+#define MO_QWER MO(0)
 
 enum layer_number
 {
@@ -123,35 +127,35 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     KC_ESC   , KC_Q     , KC_W     , KC_E     , KC_R     , KC_T     ,      KC_Y     , KC_U     , KC_I     , KC_O     , KC_P     , KC_DEL  ,
     KC_TAB   , KC_A     , KC_S     , KC_D     , KC_F     , KC_G     ,      KC_H     , KC_J     , KC_K     , KC_L     , KC_SCLN  , S(KC_7) ,
     KC_LSFT  , SFT_Z    , KC_X     , KC_C     , KC_V     , KC_B     ,      KC_N     , KC_M     , KC_COMM  , KC_DOT   , KC_SLSH  , KC_INT1 ,
-    KC_LALT  , KC_LGUI  ,   CTRL_LANG , LT(3,KC_SPC) , LT(1,KC_LNG1),      KC_BSPC  , LT(2,KC_ENT) , RCTL_T(KC_LNG2) , XXXXXXX  , XXXXXXX
+               KC_LALT  , KC_LGUI  , CTRL_LANG, LYR_SPC  , LYR_MINS ,      KC_BSPC  , LYR_ENT  , XXXXXXX  , XXXXXXX  , KC_CAPSj
   ),
 
   [_NUM] = LAYOUT_universal(
     KC_GRV   , KC_1     , KC_2     , KC_3     , KC_4     , KC_5     ,      KC_6     , KC_7     , KC_8     , KC_9     , KC_0     , _______ ,
     _______  , KC_COMM  , KC_DOT   , KC_MINS  , KC_EQL   , KC_SLSH  ,      KC_BSLS  , KC_QUOT  , KC_SCLN  , KC_LBRC  , KC_RBRC  , XXXXXXX ,
     _______  , XXXXXXX  , XXXXXXX  , XXXXXXX  , XXXXXXX  , XXXXXXX  ,      XXXXXXX  , XXXXXXX  , XXXXXXX  , XXXXXXX  , XXXXXXX  , XXXXXXX ,
-    _______  , _______  ,   _______   , _______      , _______      ,      _______  , _______      , XXXXXXX         , XXXXXXX  , XXXXXXX
+               _______  , _______  , _______  , _______  , _______  ,      _______  , _______  , XXXXXXX  , XXXXXXX  , XXXXXXX
   ),
 
   [_SFT_NUM] = LAYOUT_universal(
     SFT_GRV  , SFT_1    , SFT_2    , SFT_3    , SFT_4    , SFT_5    ,      SFT_6    , SFT_7    , SFT_8    , SFT_9    , SFT_0    , _______ ,
     KC_F1    , SFT_COMM , SFT_DOT  , SFT_MINS , SFT_EQL  , SFT_SLSH ,      SFT_BSLS , SFT_QUOT , SFT_SCLN , SFT_LBRC , SFT_RBRC , XXXXXXX ,
     _______  , XXXXXXX  , XXXXXXX  , XXXXXXX  , XXXXXXX  , XXXXXXX  ,      XXXXXXX  , XXXXXXX  , XXXXXXX  , XXXXXXX  , XXXXXXX  , XXXXXXX ,
-    _______  , _______  ,   _______   , _______      , _______      ,      _______  , _______      , XXXXXXX         , XXXXXXX  , XXXXXXX
+               _______  , _______  , _______  , _______  , _______  ,      _______  , _______  , XXXXXXX  , XXXXXXX  , XXXXXXX
   ),
 
   [_ARROW_MOUSE] = LAYOUT_universal(
     KC_F1    , KC_F2    , KC_F3    , KC_F4    , KC_F5    , KC_F6    ,      KC_F7    , KC_F8    , KC_F9    , KC_F10   , KC_F11   , KC_F12   ,
     _______  , XXXXXXX  , XXXXXXX  , KC_UP    , XXXXXXX  , KC_HOME  ,      KC_PGUP  , MS_BTN1  , MS_WHLU  , MS_BTN2  , MS_BTN3  , AML_TO   ,
     _______  , XXXXXXX  , KC_LEFT  , KC_DOWN  , KC_RGHT  , KC_END   ,      KC_PGDN  , MS_WHLL  , MS_WHLD  , MS_WHLR  , MS_BTN4  , MS_BTN5  ,
-    _______  , _______  ,   _______   , _______      , _______      ,      KC_LSFT  , KC_LCTL      , WIN_LOCK        , XXXXXXX  , XXXXXXX
+               _______  , _______  , _______  , _______  , _______  ,      KC_LSFT  , KC_LCTL  , WIN_LOCK , XXXXXXX  , XXXXXXX
   ),
 
   [_MOUSE] = LAYOUT_universal(
     XXXXXXX  , XXXXXXX  , XXXXXXX  , XXXXXXX  , XXXXXXX  , XXXXXXX  ,      UNDO     , CUT      , COPY     , PASTE    , CNG_WIN  , SCRL_DVI ,
     XXXXXXX  , XXXXXXX  , XXXXXXX  , KC_UP    , XXXXXXX  , KC_HOME  ,      KC_PGUP  , MS_BTN1  , MS_WHLU  , MS_BTN2  , MS_BTN3  , SCRL_DVD ,
     XXXXXXX  , XXXXXXX  , KC_LEFT  , KC_DOWN  , KC_RGHT  , KC_END   ,      KC_PGDN  , MS_WHLL  , MS_WHLD  , MS_WHLR  , MS_BTN4  , MS_BTN5  ,
-    XXXXXXX  , XXXXXXX  , MO(_QWERTY) , MO(_QWERTY)  , MO(_QWERTY)  ,      KC_LSFT  , KC_LCTL      , WIN_LOCK        , XXXXXXX  , XXXXXXX
+               XXXXXXX  , XXXXXXX  , MO_QWER  , MO_QWER  , MO_QWER  ,      KC_LSFT  , KC_LCTL  , XXXXXXX  , XXXXXXX  , WIN_LOCK
   ),
 };
 // clang-format on
