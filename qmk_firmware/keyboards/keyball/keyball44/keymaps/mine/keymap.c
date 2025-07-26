2b8200793a67a63a0ae45c716d949cafe1b9daa1
Kaz310 <ptarmigan.09.02@gmail.com>
2025-07-27 02:23:00 +0900

    fix mine

--- qmk_firmware/keyboards/keyball/keyball44/keymaps/mine/keymap.c
+++ qmk_firmware/keyboards/keyball/keyball44/keymaps/mine/keymap.c
@@ -163,7 +163,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 layer_state_t layer_state_set_user(layer_state_t state)
 {
   // Auto enable scroll mode when the highest layer is 3
-  keyball_set_scroll_mode(get_highest_layer(state) == _ARROW_MOUSE);
+  keyball_set_scroll_mode(get_highest_layer(state) == 10);
   return state;
 }
 
