4150969a703f3918aea6a0ad947b2d12b8b1073f
Kaz310 <ptarmigan.09.02@gmail.com>
2025-07-27 13:53:53 +0900

    fix

--- qmk_firmware/keyboards/keyball/keyball44/keymaps/mine/keymap.c
+++ qmk_firmware/keyboards/keyball/keyball44/keymaps/mine/keymap.c
@@ -118,7 +118,7 @@ bool process_record_user(uint16_t keycode, keyrecord_t *record)
 #define COPY LCTL(KC_C)
 #define PASTE LCTL(KC_V)
 #define CNG_WIN LGUI(KC_TAB)
-#define MO_QWER MO(_QWERTY)
+#define MO_QWER TG(_QWERTY)
 
 // clang-format off
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
