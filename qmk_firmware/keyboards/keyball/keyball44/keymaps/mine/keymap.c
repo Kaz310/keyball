16dc00e22ebd3740ad76f0a4fb9f269091b01ef5
Kaz310 <ptarmigan.09.02@gmail.com>
2025-08-05 20:22:35 +0900

    update key map of mine

--- qmk_firmware/keyboards/keyball/keyball44/keymaps/mine/keymap.c
+++ qmk_firmware/keyboards/keyball/keyball44/keymaps/mine/keymap.c
@@ -76,10 +76,10 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   ),
 
   [_NUM] = LAYOUT_right_ball(
-    XXXXXXX   , XXXXXXX   , KC_7      , KC_8      , KC_9      , COLON     ,      KC_F1     , KC_F2     , KC_F3     , KC_F4     , KC_F5     , KC_DEL    ,
-    PLUS      , KC_MINS   , KC_4      , KC_5      , KC_6      , KC_COMM   ,      KC_F6     , KC_F7     , KC_F8     , KC_F9     , KC_F10    , XXXXXXX   ,
-    STAR      , KC_SLSH   , KC_1      , KC_2      , KC_3      , KC_DOT    ,      XXXXXXX   , XXXXXXX   , XXXXXXX   , XXXXXXX   , XXXXXXX   , XXXXXXX   ,
-                KC_0      , KC_EQL    , _______   , _______   , _______   ,      _______   , _______                           , KC_PSCR
+    _______   , KC_1      , KC_2      , KC_3      , KC_4      , KC_5      ,      KC_6      , KC_7      , KC_8      , KC_9      , COLON     , KC_DEL    ,
+    _______   , KC_F1     , KC_F2     , KC_F3     , KC_F4     , KC_F5     ,      XXXXXXX   , KC_4      , KC_5      , KC_6      , KC_COMM   , XXXXXXX   ,
+    _______   , KC_F6     , KC_F7     , KC_F8     , KC_F9     , KC_F10    ,      KC_0      , KC_1      , KC_2      , KC_3      , KC_DOT    , XXXXXXX   ,
+                _______   , _______   , _______   , _______   , _______   ,      _______   , _______                           , XXXXXXX
   ),
 
   [_SIGN] = LAYOUT_right_ball(
